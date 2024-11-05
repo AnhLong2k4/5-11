8c26b1328ba23db20e6355793d3377d819fb96e0
AnhLong2k4 <164758904+AnhLong2k4@users.noreply.github.com>
2024-11-05 19:45:29 +0700

    Add files via upload

---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvKhachHang = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.txtTenKH = new System.Windows.Forms.TextBox();
+            this.txtSDT = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvKhachHang
+            // 
+            this.dgvKhachHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvKhachHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dgvKhachHang.Location = new System.Drawing.Point(12, 12);
+            this.dgvKhachHang.Name = "dgvKhachHang";
+            this.dgvKhachHang.Size = new System.Drawing.Size(464, 536);
+            this.dgvKhachHang.TabIndex = 0;
+            this.dgvKhachHang.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 580);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Thêm";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(106, 580);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 41);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sửa";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(200, 580);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 41);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Xoá";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(294, 580);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 41);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Hoá Đơn";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(388, 580);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(88, 41);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Chỉ Tiết";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // txtTenKH
+            // 
+            this.txtTenKH.Location = new System.Drawing.Point(495, 48);
+            this.txtTenKH.Multiline = true;
+            this.txtTenKH.Name = "txtTenKH";
+            this.txtTenKH.Size = new System.Drawing.Size(202, 37);
+            this.txtTenKH.TabIndex = 6;
+            this.txtTenKH.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtSDT
+            // 
+            this.txtSDT.Location = new System.Drawing.Point(495, 170);
+            this.txtSDT.Multiline = true;
+            this.txtSDT.Name = "txtSDT";
+            this.txtSDT.Size = new System.Drawing.Size(202, 37);
+            this.txtSDT.TabIndex = 7;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(495, 282);
+            this.txtDiaChi.Multiline = true;
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(202, 37);
+            this.txtDiaChi.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(492, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Tên Khách Hàng";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(493, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Số Điện Thoại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(493, 244);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Địa Chỉ";
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Tên Khách Hàng";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Số Điẹn Thoại";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Địa Chỉ";
+            this.Column3.Name = "Column3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(932, 633);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtSDT);
+            this.Controls.Add(this.txtTenKH);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvKhachHang);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvKhachHang)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvKhachHang;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox txtTenKH;
+        private System.Windows.Forms.TextBox txtSDT;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+    }
+}
+
---
+++ WindowsFormsApp5/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class KhachHang
+        {
+            public int MaKH { get; set; }
+            public string TenKH { get; set; }
+            public string SoDienThoai { get; set; }
+            public string DiaChi { get; set; }
+        }
+
+
+        private BindingList<KhachHang> danhSachKhachHang = new BindingList<KhachHang>();
+
+        private int nextMaKH = 1; // Tự động tăng mã khách hàng
+
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dgvKhachHang.AutoGenerateColumns = true; // Automatically create columns from data properties
+            // Thiết lập các cột
+            dgvKhachHang.Columns.Add("MaKH", "Mã KH");
+            dgvKhachHang.Columns.Add("TenKH", "Tên Khách Hàng");
+            dgvKhachHang.Columns.Add("SoDienThoai", "Số Điện Thoại");
+            dgvKhachHang.Columns.Add("DiaChi", "Địa Chỉ");
+        }
+
+
+        // Sự kiện khi chọn một dòng trong DataGridView để hiển thị thông tin vào TextBox
+        private void dgvKhachHang_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Kiểm tra xem có dòng nào được chọn không
+            if (e.RowIndex >= 0)
+            {
+                // Lấy khách hàng từ dòng được chọn
+                KhachHang khachHang = danhSachKhachHang[e.RowIndex];
+
+                // Hiển thị thông tin vào các TextBox
+                txtTenKH.Text = khachHang.TenKH;
+                txtSDT.Text = khachHang.SoDienThoai;
+                txtDiaChi.Text = khachHang.DiaChi;
+
+                // Lưu chỉ số của dòng đang chọn để sửa sau
+                selectedIndex = e.RowIndex;
+            }
+        }
+
+        // Biến lưu chỉ số của khách hàng đang được chọn để sửa
+        private int selectedIndex = -1;
+
+        // Sự kiện khi nhấn nút Sửa để cập nhật thông tin khách hàng
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem có khách hàng nào đang được chọn không
+            if (selectedIndex >= 0 && selectedIndex < danhSachKhachHang.Count)
+            {
+                // Cập nhật thông tin khách hàng trong danh sách
+                danhSachKhachHang[selectedIndex].TenKH = txtTenKH.Text;
+                danhSachKhachHang[selectedIndex].SoDienThoai = txtSDT.Text;
+                danhSachKhachHang[selectedIndex].DiaChi = txtDiaChi.Text;
+
+                // Cập nhật DataGridView
+                CapNhatDanhSachKhachHang();
+
+                // Đặt lại chỉ số dòng được chọn
+                selectedIndex = -1;
+
+                // Xóa trắng các TextBox sau khi sửa
+                txtTenKH.Clear();
+                txtSDT.Clear();
+                txtDiaChi.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng để sửa.");
+            }
+        }
+
+
+        public class DichVu
+        {
+            public int MaDV { get; set; }
+            public string TenDV { get; set; }
+            public decimal GiaTien { get; set; }
+        }
+
+
+
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Tạo đối tượng KhachHang mới từ dữ liệu nhập trong các TextBox
+            KhachHang khachHang = new KhachHang
+            {
+                MaKH = nextMaKH++, // Mã khách hàng tự động tăng
+                TenKH = txtTenKH.Text,
+                SoDienThoai = txtSDT.Text,
+                DiaChi = txtDiaChi.Text
+            };
+
+            // Thêm khách hàng vào danh sách
+            danhSachKhachHang.Add(khachHang);
+
+            // Cập nhật DataGridView
+            CapNhatDanhSachKhachHang();
+
+            // Xóa trắng các TextBox sau khi thêm
+            txtTenKH.Clear();
+            txtSDT.Clear();
+            txtDiaChi.Clear();
+        }
+
+        // Phương thức để cập nhật DataGridView hiển thị danh sách khách hàng
+        private void CapNhatDanhSachKhachHang()
+        {
+            dgvKhachHang.DataSource = null;
+            dgvKhachHang.DataSource = danhSachKhachHang;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Kiểm tra xem có dòng nào đang được chọn trong DataGridView không
+            if (dgvKhachHang.SelectedRows.Count > 0)
+            {
+                // Lấy chỉ số dòng được chọn
+                int index = dgvKhachHang.SelectedRows[0].Index;
+
+                // Xóa khách hàng từ danh sách dựa vào chỉ số dòng
+                danhSachKhachHang.RemoveAt(index);
+
+                // Cập nhật DataGridView
+                CapNhatDanhSachKhachHang();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng cần xóa.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (dgvKhachHang.SelectedRows.Count > 0)
+            {
+                int selectedIndex = dgvKhachHang.SelectedRows[0].Index;
+                KhachHang khachHang = danhSachKhachHang[selectedIndex];
+
+                MessageBox.Show($"Mã KH: {khachHang.MaKH}\nTên KH: {khachHang.TenKH}\nSĐT: {khachHang.SoDienThoai}\nĐịa chỉ: {khachHang.DiaChi}", "Chi tiết khách hàng");
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn khách hàng.", "Thông báo");
+            }
+        }
+    }
+}
